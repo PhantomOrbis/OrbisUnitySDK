0dabed549df9651f396e581fe37adecc1b497952
PhantomNeon <chho1365@gmail.com>
2023-01-20 14:54:21 +0900

    v0.0.0 update

---
+++ System/Assets/Orbis/Editor/OrbisTag.cs
@@ -0,0 +1,46 @@
+#if UNITY_EDITOR
+
+using System.Linq;
+using UnityEditor;
+using UnityEditorInternal;
+
+public class OrbisTag : Editor
+{
+    public static readonly string[] tag =
+    {        
+        "GameManager",
+        "NetworkManager"
+    };
+
+    [MenuItem("Orbis/Tag/Add Tag", priority = 101)]
+    public static void AddTag()
+    {
+        foreach(var location in tag)
+        {
+            if (location.Length == 0)
+                continue;
+
+            if (!InternalEditorUtility.tags.Contains(location))
+            {
+                InternalEditorUtility.AddTag(location);
+            }
+        }
+    }
+
+    [MenuItem("Orbis/Tag/Remove Tag", priority = 102)]
+    public static void RemoveTag()
+    {
+        foreach (var location in tag)
+        {
+            if (location.Length == 0)
+                continue;
+
+            if (!InternalEditorUtility.tags.Contains(location))
+            {
+                InternalEditorUtility.RemoveTag(location);
+            }            
+        }
+    }
+}
+
+#endif
