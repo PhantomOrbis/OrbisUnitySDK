de4cf7c57a2e097b8df64623c94d18abd69f2795
PhantomNeon <chho1365@gmail.com>
2023-01-25 00:47:20 +0900

    v0.0.1 update

--- System/Assets/Orbis/Editor/OrbisTag.cs
+++ System/Assets/Orbis/Editor/OrbisTag.cs
@@ -3,19 +3,23 @@
 using System.Linq;
 using UnityEditor;
 using UnityEditorInternal;
+using UnityEngine;
 
-public class OrbisTag : Editor
+public class OrbisTag
 {
     public static readonly string[] tag =
     {        
         "GameManager",
         "NetworkManager"
     };
+}
 
-    [MenuItem("Orbis/Tag/Add Tag", priority = 101)]
-    public static void AddTag()
+public class OrbisTagMenu : Editor
+{
+    [MenuItem("Orbis/Tag/Add Tag", priority = 121)]
+    private static void AddTag()
     {
-        foreach(var location in tag)
+        foreach (var location in OrbisTag.tag)
         {
             if (location.Length == 0)
                 continue;
@@ -27,10 +31,10 @@ public class OrbisTag : Editor
         }
     }
 
-    [MenuItem("Orbis/Tag/Remove Tag", priority = 102)]
-    public static void RemoveTag()
+    [MenuItem("Orbis/Tag/Remove Tag", priority = 122)]
+    private static void RemoveTag()
     {
-        foreach (var location in tag)
+        foreach (var location in OrbisTag.tag)
         {
             if (location.Length == 0)
                 continue;
@@ -38,8 +42,38 @@ public class OrbisTag : Editor
             if (InternalEditorUtility.tags.Contains(location))
             {
                 InternalEditorUtility.RemoveTag(location);
-            }            
+            }
         }
+    }
+}
+
+[InitializeOnLoad]
+public class OrbisTagWindow : EditorWindow
+{  
+    [MenuItem("Orbis/Tag/Tag Manager", priority = 101)]
+    private static void TagWindow()
+    {
+        /*
+         * utility[bool]
+         * [true] = floating utility window
+         * [false ] = normal window
+         */
+        OrbisTagWindow window = (OrbisTagWindow)EditorWindow.GetWindow(typeof(OrbisTagWindow), false, "Tag Manager");
+        window.Show();        
+    }
+
+    private void OnGUI()
+    {
+        OrbisTagWindow window = (OrbisTagWindow)EditorWindow.GetWindow(typeof(OrbisTagWindow));
+        //Debug.Log("x : " + window.position.size.x);
+        //Debug.Log("y : " + window.position.size.y);
+        GUILayout.BeginArea(new Rect(20, 20, 4000, 4000));
+
+        GUILayout.BeginVertical("box", GUILayout.MaxWidth(200), GUILayout.MaxHeight((window.position.size.y - 20)));             
+        GUILayout.Label("Side NavBar", EditorStyles.boldLabel);        
+        GUILayout.EndVertical();
+        
+        GUILayout.EndArea();        
     }
 }
 
