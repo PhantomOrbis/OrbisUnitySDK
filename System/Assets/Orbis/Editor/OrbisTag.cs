892bd9072914e628df960308ba81717c7e70a0bf
PhantomNeon <chho1365@gmail.com>
2023-02-01 23:52:48 +0900

    v0.0.0 - update * commit

--- System/Assets/Orbis/Editor/OrbisTag.cs
+++ System/Assets/Orbis/Editor/OrbisTag.cs
@@ -1,5 +1,6 @@
 #if UNITY_EDITOR
 
+using System.Collections.Generic;
 using System.Linq;
 using UnityEditor;
 using UnityEditorInternal;
@@ -50,6 +51,9 @@ public class OrbisTagMenu : Editor
 [InitializeOnLoad]
 public class OrbisTagWindow : EditorWindow
 {  
+    private static List<string> tag = new List<string>();
+
+
     [MenuItem("Orbis/Tag/Tag Manager", priority = 101)]
     private static void TagWindow()
     {
@@ -59,7 +63,15 @@ public class OrbisTagWindow : EditorWindow
          * [false ] = normal window
          */
         OrbisTagWindow window = (OrbisTagWindow)EditorWindow.GetWindow(typeof(OrbisTagWindow), false, "Tag Manager");
-        window.Show();        
+        window.Show();               
+    }
+
+    private static void SetTag()
+    {
+        foreach(var code in InternalEditorUtility.tags)
+        {
+            tag.Add(code);
+        }
     }
 
     private void OnGUI()
@@ -67,12 +79,51 @@ public class OrbisTagWindow : EditorWindow
         OrbisTagWindow window = (OrbisTagWindow)EditorWindow.GetWindow(typeof(OrbisTagWindow));
         //Debug.Log("x : " + window.position.size.x);
         //Debug.Log("y : " + window.position.size.y);
-        GUILayout.BeginArea(new Rect(20, 20, 4000, 4000));
+        GUILayout.BeginArea(new Rect(20, 20, 3960, 3960));
+        GUILayout.BeginVertical("box", GUILayout.MaxWidth(200), GUILayout.MaxHeight((window.position.size.y - 40)));
+        {
+            GUILayout.Space(10);
 
-        GUILayout.BeginVertical("box", GUILayout.MaxWidth(200), GUILayout.MaxHeight((window.position.size.y - 20)));             
-        GUILayout.Label("Side NavBar", EditorStyles.boldLabel);        
-        GUILayout.EndVertical();
+            GUILayout.BeginHorizontal();
+            GUILayout.FlexibleSpace();
+
+            
+            GUILayout.Label("Tag");
+            {
+                GUI.skin.label.fontSize = 16;
+                GUI.skin.label.fontStyle = FontStyle.Bold;
+            }
+            GUILayout.FlexibleSpace();            
+            GUILayout.EndHorizontal();            
+            GUILayout.Space(10);
+                        
+            EditorGUI.DrawRect(EditorGUILayout.GetControlRect(false, 1f), Color.gray);
+
+            if (tag.Count != InternalEditorUtility.tags.Count())
+            {
+                foreach (var code in InternalEditorUtility.tags)
+                {
+                    tag.Add(code);
+                }
+            }
+
+            GUILayout.Space(10);
+
+            for (int i = 0; i < tag.Count; i++)
+            {
+                GUILayout.Space(2);
+                GUILayout.BeginHorizontal();
+                GUILayout.FlexibleSpace();
+                if (GUILayout.Button(tag[i], GUILayout.Width(180F)))
+                {
+                    Debug.Log(tag[i]);
+                }
+                GUILayout.FlexibleSpace();
+                GUILayout.EndHorizontal();
+            }
+        }
         
+        GUILayout.EndVertical();        
         GUILayout.EndArea();        
     }
 }
